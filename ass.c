a59335af79e43b4c1d0117fab8d2dbd1aa945991
Oyerinde Joshua <48280734+joshuaoyerinde@users.noreply.github.com>
2021-10-21 17:00:46 +0100

    third commited on c programmming

---
+++ ass.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// int main()
+// {
+
+//  int i,j,k;
+
+//  for(i=0, j=0, k=0; i<4, k<8, j<10; i++)
+//  {
+
+//  printf("%d %d %d\n",i,j,k);
+//     j+=2;
+//     k+=3;
+ 
+//  }
+// }
+// #include <stdio.h>
+void main ()
+{
+ int i=0,j=2;
+ for(i = 0;i<5;i++,j=j+2)
+ {
+ printf("%d %d\n",i,j);
+ }
+} 
